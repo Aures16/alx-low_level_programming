96156823811b1c94a488f155c297e43bcb3d1234
Aures16 <medoaures@gmail.com>
2023-07-13 16:16:59 -0700

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
-#include <studio.h>
-
+#include <stdio.h>
 /**
  * main - Entry point
  * Return: Always return 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");        return (0);
